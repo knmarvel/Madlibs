d2ff8f4d4273ea05597fbeb55727ab2f15c70f62
Karen Marvel <knmarvel@gmail.com>
2019-08-06 11:57:16 -0400

    Madlibs

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let yfm = prompt("What's your favorite movie?");
+let fp = prompt("Tell me the name of a person you know or know about, living or dead, real or fictional:");
+let act = prompt("Tell me an action:");
+let adjing = prompt("Tell me a word ending in -ing:");
+let pluralnn = prompt("Tell me a plural noun:");
+let place = prompt("Tell me the name of a place:");
+let verb = prompt("Tell me an action verb:")
+let object = prompt("Tell me the name of an object:");
+let adj = prompt("Tell me an adjective:");
+let occ = prompt("Tell me an occupation:");
+let adj2 = prompt("Tell me another adjective:");
+let singularnn = prompt("Tell me a singular noun:");
+document.write("A Quick Synopsis of " + yfm + "!" );
+document.write(yfm," opens with ", fp, " doing the ", act, " to a sweet, ",adjing, " beat! After impressing all the " , pluralnn, " in ", place, " with dance, ",fp, verb, " the ", place,"’s ",object,". Everyone is ", adj,"! It turns out that ",fp," was actually the ", occ," all along. The ",pluralnn," find ", adj2," love and ", fp," disappears into the ", singularnn,".")
